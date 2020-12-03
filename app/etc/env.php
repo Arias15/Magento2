5a1ffcc52fe4e5d4e0078ef0d7e57fade70c5717
Arias15 <gariasc@uni.pe>
2020-12-03 05:01:25 +0000

    magento html

---
+++ app/etc/env.php
@@ -0,0 +1,88 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '563921edcc0380afb4306fe05d435558'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => '8cb37c743940d743c21b93b78c46392794b879d0229d0ce2',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'production',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => false
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'testraumi.xyz'
+    ],
+    'install' => [
+        'date' => 'Mon, 30 Nov 2020 22:29:43 +0000'
+    ],
+    'http_cache_hosts' => [
+        [
+            'host' => 'testraumi.xyz'
+        ]
+    ]
+];
